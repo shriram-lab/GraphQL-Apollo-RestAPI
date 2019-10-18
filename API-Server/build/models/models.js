c5be75234c665c389e4d59f5bded5146135a925c
shri <shri@gmail.com>
2019-10-18 18:32:57 +0530

    Apollo server and REST api server

--- API-Server/build/models/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const UserModelSchema_1 = __importDefault(require("./User/UserModelSchema"));
-exports.Users = mongoose_1.default.model('Users', UserModelSchema_1.default);
-//# sourceMappingURL=models.js.map
